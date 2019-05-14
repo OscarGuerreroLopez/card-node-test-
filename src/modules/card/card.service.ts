c431e3dc77633598f22515ee8b466a87dacc8f1e
Oscar Guerrero <oscar.computer.guy@gmail.com>
2019-05-14 14:59:20 +0200

    encryption working

--- src/modules/card/card.service.ts
+++ src/modules/card/card.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { ICardInfo, ICardInfoItem } from './interfaces/index';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class CardService {
@@ -10,7 +11,15 @@ export class CardService {
   ) {}
   storeCardInfo(cardInfo: ICardInfo): Promise<ICardInfoItem> {
     return new Promise((resolve, reject) => {
-      const newCard = new this.cardModel(cardInfo);
+      // tslint:disable-next-line:no-console
+      console.log(cardInfo);
+      const cardInfoHashed: ICardInfo = {
+        ccNumber: this.encryptIt(cardInfo.ccNumber),
+        ccCvv: this.encryptIt(cardInfo.ccCvv),
+        ccName: cardInfo.ccName,
+        ccExpiration: cardInfo.ccExpiration,
+      };
+      const newCard = new this.cardModel(cardInfoHashed);
 
       newCard
         .save()
@@ -21,5 +30,12 @@ export class CardService {
           reject(err);
         });
     });
+  }
+
+  encryptIt(item: string): string {
+    const salt = bcrypt.genSaltSync(10);
+    const hashItem = bcrypt.hashSync(item, salt);
+
+    return hashItem;
   }
 }
